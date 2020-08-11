9859c72004e1ae6101ff49188806d29bd4a6051f
delxcign <57690820+delxcign@users.noreply.github.com>
2020-08-11 13:39:43 +0700

    Initial commit

---
+++ app/src/main/java/id/sch/smktelkom/intentv2/act3.java
@@ -0,0 +1,14 @@
+package id.sch.smktelkom.intentv2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class act3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_act3);
+    }
+}
